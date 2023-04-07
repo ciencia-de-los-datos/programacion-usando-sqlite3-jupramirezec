ead359ef04bd1dc3cdd233e20ef4bcdc2928404d
jupramirezec <36797104+jupramirezec@users.noreply.github.com>
2023-04-07 13:42:30 -0500

    Octava revision

--- pregunta_08.sql
+++ pregunta_08.sql
@@ -41,4 +41,8 @@
 --
 --  >>> Escriba su codigo a partir de este punto <<<
 --
-select substr(c23,1,4) as 'YEAR' , round(avg(c21),6) as 'avg(c21)' from tbl2 group by YEAR
+SELECT strftime('%Y', c23) as 'YEAR', round(avg(c21),6) as 'avg(c21)'
+FROM tbl2
+GROUP BY YEAR
+ORDER BY ASC
+--select substr(c23,1,4) as 'YEAR' , round(avg(c21),6) as 'avg(c21)' from tbl2 group by YEAR
