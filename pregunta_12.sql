71f0eb1af04a15230c0f5491fe1d60e560f5f4dd
jupramirezec <36797104+jupramirezec@users.noreply.github.com>
2023-04-07 13:33:13 -0500

    septima revision

--- pregunta_12.sql
+++ pregunta_12.sql
@@ -42,4 +42,7 @@
 --
 --  >>> Escriba su codigo a partir de este punto <<<
 --
-select K0, round(MAX(c12), 2) as 'MAX(c12)', round(min(c12),2) as 'min(C12)' from tbl1 group by K0
+
+select K0, MAX(C12) , min(C12) from tbl1 group by K0
+
+--select K0, round(MAX(c12), 2) as 'MAX(c12)', round(min(c12),2) as 'min(C12)' from tbl1 group by K0
